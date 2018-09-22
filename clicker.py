a52967d9dcadc8752abc62353efa84fb12ad1ac5
Jędrzej <43277806+BasileusRomanum@users.noreply.github.com>
2018-09-22 15:49:52 +0200

    Version 0.1
    One file and that's all, folks. I'd love to make it work better. Like, using start button to stop clicking, so you can change values, but I don't really know how to do it yet.

---
+++ clicker.py
@@ -0,0 +1,102 @@
+import time
+import random
+import sys
+
+from pyautogui import click
+from tkinter import *
+from tkinter.messagebox import showinfo
+from multiprocessing import Process
+#Flag used to checking if the button was triggered for starting or stopping
+#Doesn't really work as of now.
+flag = False
+button_state = 'normal'
+
+def Help():
+    showinfo(title='Help', message="Default value for minimum is: 1 second. For maximum it's: 5 seconds")
+
+def intestines():
+    global flag
+
+    time.sleep(1)
+
+    if flag == False:
+        flag = True
+    else:
+        flag = False
+
+    try:
+        min = int(entry1['textvariable'])
+    except ValueError:
+        min = 0
+    try:
+        max = int(entry2['textvariable']) + 1
+    except ValueError:
+        max = 6
+
+    if min > max:
+        showinfo(title="Error", message="Maximum value is smaller than minimum value.")
+        return
+    else:
+        pass
+
+    while True:
+        i = 0
+        time_ = (20 + random.randint(min, max)) / 10
+        for i in range(0, time_):
+            time.sleep(1)
+        click()
+
+def clicki():
+    global x
+    x = Process(target=intestines)
+    x.start()
+
+def unenter():
+    global x
+    x.terminate()
+    sys.exit(0)
+
+
+win = Tk()
+win.title("Autoclicker")
+
+button_click = Button(win,
+                      text='Start',
+                      command=clicki)
+button_help = Button(win,
+                     text='Help',
+                     command=Help)
+button_quit = Button(win,
+                     text='Quit',
+                     command=unenter)
+entry1 = Entry(win,
+               textvariable=min,
+               width=5)
+entry2 = Entry(win,
+               textvariable=max,
+               width=5)
+label1 = Label(win,
+               text='Minimum')
+label2 = Label(win,
+               text='Maksimum')
+
+def clickerino():
+    label1.grid(row=0, column=0)
+    label2.grid(row=0, column=1)
+    entry1.grid(row=1, column=0)
+    entry2.grid(row=1, column=1)
+    button_quit.grid(row=3, column=1)
+    button_help.grid(row=2, column=1)
+    clickit = Process(group=None,
+                      target=intestines,
+                      name=None,
+                      args=(),
+                      kwargs={}
+                      )
+    button_click.grid(row=2, column=0)
+
+    win.mainloop()
+
+if __name__ == '__main__':
+    begin = Process(group=None, target=clickerino, name=None, args=(), kwargs={})
+    begin.start()
